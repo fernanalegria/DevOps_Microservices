1819c129af855fe11d69d615a4a8a78f8a8df8d2
Ubuntu <ubuntu@ip-172-31-16-118.us-east-2.compute.internal>
2020-04-18 19:19:50 +0000

    feat: Create shell script to upload the Docker image to the registry

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,18 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+DOCKER_PATH="fernanalegria/boston-housing"
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+cat ~/docker_access_token.txt | docker login --username fernanalegria --password-stdin docker.io
+docker tag boston-housing:latest $DOCKER_PATH:latest
+echo "Docker ID and Image: $DOCKER_PATH"
 
 # Step 3:
 # Push image to a docker repository
+docker push $DOCKER_PATH:latest
+
+# Step 4:
+# Log out of Docker Hub
+docker logout
