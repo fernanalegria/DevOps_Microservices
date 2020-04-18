f22e04f0a7ea8ad65df55b9a3e5e810c2995d4b1
Ubuntu <ubuntu@ip-172-31-16-118.us-east-2.compute.internal>
2020-04-18 22:51:58 +0000

    feat: Create shell script to deploy the app to k8s

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,30 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+DOCKER_PATH="fernanalegria/boston-housing"
 
-# Step 2
-# Run the Docker Hub container with kubernetes
-
+# Step 2:
+# Clean previous deployments
+if [ $(kubectl get pod boston-housing-pod -o jsonpath='{.kind}') ]
+  then
+    kubectl delete pod boston-housing-pod
+fi
 
 # Step 3:
-# List kubernetes pods
+# Run the Docker Hub container with kubernetes
+kubectl run boston-housing-pod \
+    --image=$DOCKER_PATH \
+    --port=8000 \
+    --labels app=boston-housing
 
 # Step 4:
-# Forward the container port to a host
+# List kubernetes pods
+kubectl get pods
 
+# Step 5:
+# Forward the container port to a host
+while [ $(kubectl get pod boston-housing-pod -o jsonpath="{.status.phase}") != "Running" ]; do
+    echo "Deploying Boston housing pod..."
+done
+kubectl get pod boston-housing-pod
+kubectl port-forward boston-housing-pod 8000:8000
